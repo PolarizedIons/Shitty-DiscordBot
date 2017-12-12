26f10a57ef041ca9e6c44b73e78ed256f2f0c726
PolarizedIons <PolarizedIons@gmail.com>
2017-12-12 15:14:00 +0200

    Check that dependencies are installed, and a config file exists

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-const winston = require('winston');
-winston.default.transports.console.level='debug';
-winston.transports.Console.level = 'debug';
+try {
+    const winston = require('winston');
+    winston.default.transports.console.level='debug';
+    winston.transports.Console.level = 'debug';
+} catch (err) {
+    if (err.code !== "MODULE_NOT_FOUND") {
+        throw err;
+    }
+
+    console.error("Remember to install dependencies!");
+    process.exit(1);
+}
 
 
-// Do stuff here
+try {
+    require('./config')
+} catch (err) {
+    if (err.code !== "MODULE_NOT_FOUND") {
+        throw err;
+    }
+
+    console.error("Remember to setup your config.js from config.example.js!");
+    process.exit(2);
+}
 
 
 const DiscordClient = require('./DiscordClient');
